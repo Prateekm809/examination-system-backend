ba997f3f838ff9092708de79aca623583078b683
Prateekm809 <122430022+Prateekm809@users.noreply.github.com>
2024-09-15 15:06:11 +0530

    Update WebConfig.java

--- src/main/java/com/project/examportalbackend/configurations/WebConfig.java
+++ src/main/java/com/project/examportalbackend/configurations/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow only the specified origin
+                .allowedOrigins("https://examination-system-frontend.vercel.app") // Allow only the specified origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow sending credentials (cookies, authorization headers, etc.)
