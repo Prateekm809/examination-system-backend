8f05882f5254463e58507dee7b160257eeac0d7f
prateekm809 <pm6837759@gmail.com>
2024-09-16 20:29:38 +0530

    deny all other than who verified

--- src/main/java/com/project/examportalbackend/configurations/SecurityConfig.java
+++ src/main/java/com/project/examportalbackend/configurations/SecurityConfig.java
@@ -39,41 +39,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+    
         http.cors();
         http.csrf().disable()
                 .authorizeRequests()
-
+    
+                // Publicly accessible endpoints
                 .antMatchers("/api/register").permitAll()
                 .antMatchers("/api/login").permitAll()
-
+                .antMatchers("/api/statistics/total").permitAll()  // Allow access to statistics endpoint
+    
+                // Restricted endpoints for specific roles
                 .antMatchers(HttpMethod.POST, "/api/category/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/category/**").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/category/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/api/category/**").hasAuthority("ADMIN")
-
+    
                 .antMatchers(HttpMethod.POST, "/api/quiz/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/quiz/**").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/quiz/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/api/quiz/**").hasAuthority("ADMIN")
-
+    
                 .antMatchers(HttpMethod.POST, "/api/question/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/question/**").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/question/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/api/question/**").hasAuthority("ADMIN")
-
+    
                 .antMatchers(HttpMethod.POST, "/api/quizResult/**").hasAuthority("USER")
                 .antMatchers(HttpMethod.GET, "/api/quizResult/all/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/quizResult/**").hasAnyAuthority("USER", "ADMIN")
-
+    
+                // Deny all other requests
                 .anyRequest().denyAll()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        ;
-
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    
+        // Add JWT filter
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
-
+   
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
