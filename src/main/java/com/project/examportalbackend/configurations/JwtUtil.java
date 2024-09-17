f027d3b9884256fd836d8d18e6ddf98a050b5265
prateekm809 <pm6837759@gmail.com>
2024-09-17 22:34:24 +0530

    added reset password and added enviroment variables

--- src/main/java/com/project/examportalbackend/configurations/JwtUtil.java
+++ src/main/java/com/project/examportalbackend/configurations/JwtUtil.java
@@ -13,7 +13,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtUtil {
-    private final String SECRET_KEY = "${SECRET_KEY}";
+    private final String SECRET_KEY = "exam-portal";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
--- src/main/java/com/project/examportalbackend/configurations/WebConfig.java
+++ src/main/java/com/project/examportalbackend/configurations/WebConfig.java
@@ -4,15 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.beans.factory.annotation.Value;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
                 .allowedOrigins(
-                    "${FRONTEND_URL}",
+                    frontendUrl,
                     "http://localhost:3000" // Allow localhost for development
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
